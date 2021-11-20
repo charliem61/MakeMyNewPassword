4d777def9c06d1fcc8d6a1e121ecd1df4737d49c
Mason Walker <masonwalker30@gmail.com>
2021-11-20 15:47:45 -0800

    addedLettersNumbersSymbolVars

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 // Assignment code here
+function generatePassword(){
+  var letters "a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z";
+  var numbers "1,2,3,4,5,6,7,8,9,0";
+  var symbols "!,@,#,$,%,^,&,*,(,),_,+,=,],[,?,>,<,~";
 
+  return "hello";
+}
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
